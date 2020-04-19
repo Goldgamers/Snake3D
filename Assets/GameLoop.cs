1be83f4c0a202bf128fb5f92fea907726cd19730
Goldgamer <max-vincent@goldgamer.de>
2020-04-19 15:44:55 +0200

    cleaner code and stuff

---
+++ Assets/GameLoop.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine.UI;
+
+public class GameLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject fruit;
+    public GameObject head;
+    List<GameObject> snakeParts;
+    public TextMesh gamescore;
+    private static int scoreCount;
+    // Renderer newHeadc;
+    public static int lastDeathReason;
+    public static bool isDeath;
+    int x, y; // for random fruit spawning
+    private const float Speed = 0.10f;
+    private int currentDirecton = 2; // clockwise 1 UP 2 RIGHT 3 DOWN 4 LEFT 
+    private GameObject newHead;
+    
+
+    public static int GetScore()
+    {
+        return scoreCount;
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+        isDeath = false;
+        lastDeathReason = 0;
+        fruit = GameObject.FindGameObjectWithTag("fruit");
+        x = Random.Range(1, 63);
+        y = Random.Range(-24, 24);
+        var newPos = new Vector3(x, y, 0);
+        fruit.transform.position = newPos;
+
+        snakeParts = new List<GameObject> {head};
+
+        var clone1 = Instantiate(head);
+        clone1.name = "init_belly";
+
+        var clonePos = head.transform.position;
+        clonePos.x--;
+        clone1.transform.position = clonePos;
+        snakeParts.Add(clone1);
+
+        var clone2 = Instantiate(head);
+        clone2.name = "init_tail";
+
+        clonePos.x--;
+        clone2.transform.position = clonePos;
+        snakeParts.Add(clone2);
+        StartCoroutine(Walk());
+        scoreCount = 0;
+
+
+        // newHeadc = newHead.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ChangeDirection();
+        gamescore.text = "Score: " + scoreCount;
+    }
+
+    private IEnumerator Walk()
+    {
+        while (true)
+        {
+            newHead = Instantiate(snakeParts[0]);
+            newHead.name = "clone_body";
+            var posChange = GetDirectionV();
+            newHead.transform.position = newHead.transform.position + posChange;
+            snakeParts.Insert(0, newHead);
+            Destroy(snakeParts[snakeParts.Count - 1]);
+            snakeParts.Remove(snakeParts[snakeParts.Count - 1]);
+            yield return new WaitForSeconds(Speed);
+
+        }
+        // ReSharper disable once IteratorNeverReturns
+    }
+
+    private Vector3 GetDirectionV()
+    {
+        var posChange = new Vector3(0, 0, 0);
+        switch (currentDirecton)
+        {
+            case 4:
+                posChange.x = -1;
+                break;
+            case 2:
+                posChange.x = 1;
+                break;
+            case 1:
+                posChange.y = 1;
+                break;
+            case 3:
+                posChange.y = -1;
+                break;
+        }
+        return posChange;
+    }
+
+    private void ChangeDirection()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && currentDirecton != 2)
+        {
+            currentDirecton = 4;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && currentDirecton != 4)
+        {
+            currentDirecton = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && currentDirecton != 3)
+        {
+            currentDirecton = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && currentDirecton != 1)
+        {
+            currentDirecton = 3;
+        }
+    }
+    public void AddHead()
+    {
+        var clone = Instantiate(snakeParts[0]);
+        clone.name = "clone_offspring";
+        var newPos = GetDirectionV();
+        clone.transform.position = clone.transform.position + newPos;
+        snakeParts.Insert(0, clone);
+        // newHeadc.material.SetColor("_Color", Random.ColorHSV());
+    }
+    public void AddScore(int scoreAdd)
+    {
+        scoreCount += scoreAdd;
+    }
+    public void NewFruit()
+    {
+        x = Random.Range(1, 63);
+        y = Random.Range(-24, 24);
+        Debug.Log(x + " / " + y);
+        var newPos = new Vector3(x, y, 0);
+        fruit.transform.position = newPos;
+
+    }
+
+}
--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,24 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
-    // public GameObject cameraWithScript;
+    public TextMesh deathReason;
     public TextMesh gameScore;
-    gameLoop GameLoop;
 
+    private bool showDeathScreen;
+
+    
     // This is to load the next Scene
     public void TransitionToMainGame()
     {
-
-
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
-
-
     }
 
 
@@ -28,15 +24,39 @@ public class MainMenu : MonoBehaviour
 
         Debug.Log("Application has been terminated");
         Application.Quit();
-
     }
 
 
-
-
-    void Start()
+    private void Start()
     {
-        gameScore.text = "Score: " + gameLoop.scoreCount;
-    }
+        var lastDeathReason = GameLoop.lastDeathReason;
+        try
+        {
+            showDeathScreen = GameLoop.isDeath;
+            GameLoop.GetScore();
+            gameScore.text = "Score: " + GameLoop.GetScore();
+        }
+#pragma warning disable 168
+        catch (UnityException e){}
+#pragma warning restore 168
+        if (!showDeathScreen)
+        {
+            deathReason.text = "Get going and score some points!";
+        }
+        else
+        {
+            Debug.Log(lastDeathReason);
+            switch (lastDeathReason)
+            {
+                case 1:
+                    deathReason.text = "Smashed into a bar";
+                    break;
+                case 2:
+                    deathReason.text = "Bit your own tail";
+                    break;
+            }
+        }
 
+
+    }
 }
